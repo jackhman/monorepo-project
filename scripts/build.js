fac4927852d68265bc60ef299aedbc0fa87949f5
zhao.liu <zhao.liu@wayz.ai>
2023-08-14 14:46:40 +0800

    :art: 添加打包的并行方法, 使用execa

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,22 @@
 const fs = require("fs")
-const path = require("path")
-
+const execa = require("execa")
 const dirs = fs.readdirSync("packages").filter(p => fs.statSync(`packages/${p}`).isDirectory())
+
+async function build(target) {
+  console.log(target)
+  await execa("rollup", ["-c", "--environment", `TARGET:${target}`])
+}
+
+async function runParallel(dirs, build) {
+  const result = []
+  for (const dir of dirs) {
+    result.push(build(dir))
+  }
+  return Promise.all(result)
+}
+
+runParallel(dirs, build).then(res=> {
+  console.log(res)
+})
 
 console.log(dirs)
