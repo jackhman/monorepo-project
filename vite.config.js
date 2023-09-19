0ee0bc863c487ecb96e39ec84b9b552d82aea3ca
zhao.liu <zhao.liu@wayz.ai>
2023-09-19 09:59:28 +0800

    :fire:使用pnpm替换yarn, 并且添加px转换为viewport的插件

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+
+// 配置 vant的 按需引入组件样式
+import Components from "unplugin-vue-components/vite"
+import { VantResolver } from "@vant/auto-import-resolver"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [VantResolver()]
+    })
+  ]
 })
