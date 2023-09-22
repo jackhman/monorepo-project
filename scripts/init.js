c4e446faa5a28beb79f2b36c3567afd6837eb9d8
zhao.liu <zhao.liu@wayz.ai>
2023-09-22 10:14:03 +0800

    :fire: 添加 nest服务端

--- scripts/init.js
+++ scripts/init.js
@@ -1,5 +1,6 @@
 import { execSync } from "child_process"
 import chalk from "chalk"
+import vscode from "vscode"
 const moduleUitls = ["pnpm", "yarn", "npm"]
 const leng = moduleUitls.length
 
@@ -14,6 +15,15 @@ for (let index = 0; index < leng; index++) {
     console.log(chalk.yellow("开始安装依赖: "))
     execSync(`${element} install`, { stdio: "inherit" })
     console.log(chalk.yellow("依赖安装完成!!!!"))
+
+    console.log(chalk.blue("开始启动 vite 前端项目"))
+    execSync(`code --list-extensions`, { stdio: "inherit" })
+    vscode.commands.executeCommand('workbench.action.terminal.split', ['npm run dev']);
+    vscode.commands.executeCommand('workbench.action.terminal.sendSequence', { 
+      text: `${element} run dev\r` 
+    });
+    // execSync(``, { stdio: "inherit" })
+
     break
   } catch (error) {
     console.log(chalk.red(`${element}可能有问题, 开始使用其他包管理器`))
