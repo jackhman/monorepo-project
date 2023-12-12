7eedbbc5ae6852878ec666d0758b3ee7c36e22e1
zhao.liu <zhao.liu@wayz.ai>
2023-12-12 11:30:25 +0800

    :art: 因为ts的原因, 可以删除多余React的依赖引入

--- vite-react-admin/src/views/dashboard/components/TodoList.tsx
+++ vite-react-admin/src/views/dashboard/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React, { useRef} from 'react'
+import { useRef} from 'react'
 import { Button, Input, List } from 'antd'
 import type { InputRef } from 'antd';
 
--- vite-react-admin/src/views/dashboard/index.tsx
+++ vite-react-admin/src/views/dashboard/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Row, Col } from 'antd'
 import './index.scss'
 import HomeCard from './components/HomeCard'
