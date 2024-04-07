1e5aade90b1a348ea9a226f343efd47a145aa990
zhao.liu <zhao.liu@wayz.ai>
2024-04-07 11:27:11 +0800

    :art: 添加echart的配置文件

--- vite-react-admin/src/views/dashboard/index.tsx
+++ vite-react-admin/src/views/dashboard/index.tsx
@@ -1,7 +1,8 @@
-import { Row, Col } from 'antd'
-import './index.scss'
-import HomeCard from './components/HomeCard'
-import TodoList from './components/TodoList'
+import { Row, Col } from "antd"
+import "./index.scss"
+import HomeCard from "./components/HomeCard"
+import TodoList from "./components/TodoList"
+import EchartsCom from "@/components/EchartsCom"
 const HomeDom = () => {
   function insertTodoList(v) {
     console.log(v)
@@ -15,15 +16,31 @@ const HomeDom = () => {
         <Row className="echart-component-main" gutter={10}>
           <Col xs={{ span: 24 }} xl={{ span: 8 }} className="echart-item-col">
             <div className="echart-item-div">
+              <EchartsCom
+                options={{
+                  xAxis: {
+                    type: "category",
+                    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+                  },
+                  yAxis: {
+                    type: "value"
+                  },
+                  series: [
+                    {
+                      data: [820, 932, 901, 934, 1290, 1330, 1320],
+                      type: "line",
+                      smooth: true
+                    }
+                  ]
+                }}
+              ></EchartsCom>
             </div>
           </Col>
           <Col xs={{ span: 24 }} xl={{ span: 8 }} className="echart-item-col">
-            <div className="echart-item-div">
-            </div>
+            <div className="echart-item-div"></div>
           </Col>
           <Col xs={{ span: 24 }} xl={{ span: 8 }} className="echart-item-col">
-            <div className="echart-item-div">
-            </div>
+            <div className="echart-item-div"></div>
           </Col>
         </Row>
       </div>
