09afcba13319efac71e8271d6866975482fed821
zhao.liu <zhao.liu@wayz.ai>
2024-04-12 10:18:23 +0800

    :art: 修改雷达图的配置文件

--- vite-react-admin/src/views/dashboard/components/EchartsCard/echartOptions.ts
+++ vite-react-admin/src/views/dashboard/components/EchartsCard/echartOptions.ts
@@ -5,122 +5,64 @@ export const autoPlayBarEchartOption = () => {}
 
 /** 雷达图 */
 export const radarEchartOption = (): EChartsOption => {
-  const legendData = ["车辆数", "设计车位"] //图例
   const indicator = [
     {
-      text: "小型车",
-      max: 6000
+      text: "A",
+      max: 100
     },
     {
-      text: "中型车",
-      max: 5000
+      text: "B",
+      max: 100
     },
     {
-      text: "大型车",
-      max: 5000
+      text: "C",
+      max: 100
     },
     {
-      text: "货车",
-      max: 5000
+      text: "D",
+      max: 100
     },
     {
-      text: "特种车",
-      max: 5000
-    },
-    {
-      text: "贵宾车",
-      max: 5000
+      text: "E",
+      max: 100
     }
   ]
   const dataArr: RadarSeriesDataItemOption[] = [
     {
-      value: [4300, 4700, 3600, 3900, 3800, 4200],
+      value: [88, 85, 88, 65, 90],
       itemStyle: {
-        color: "#4A99FF",
-        shadowColor: "#4A99FF",
+        color: "#33a3dc",
+        shadowColor: "#33a3dc",
         shadowBlur: 10
       },
       areaStyle: {
         // 单项区域填充样式
-        color: {
-          type: "linear",
-          x: 0, //右
-          y: 0, //下
-          x2: 1, //左
-          y2: 1, //上
-          colorStops: [
-            {
-              offset: 0,
-              color: "#4A99FF"
-            },
-            {
-              offset: 0.5,
-              color: "rgba(0,0,0,0)"
-            },
-            {
-              offset: 1,
-              color: "#4A99FF"
-            }
-          ],
-          global: false
-        },
-        opacity: 1 // 区域透明度
-      }
+        color: "#33a3dc",
+        opacity: 0.5 // 区域透明度
+      },
+      
     },
     {
-      value: [3200, 3000, 3400, 2000, 3900, 2000],
-      name: legendData[1],
+      value: [83, 87, 86, 60, 88],
       itemStyle: {
-        color: "#4BFFFC",
-        shadowColor: "#4BFFFC",
+        color: "#7bbfea",
+        shadowColor: "#7bbfea",
         shadowBlur: 10
       },
       areaStyle: {
         // 单项区域填充样式
-        color: {
-          type: "linear",
-          x: 0, //右
-          y: 0, //下
-          x2: 1, //左
-          y2: 1, //上
-          colorStops: [
-            {
-              offset: 0,
-              color: "#4BFFFC"
-            },
-            {
-              offset: 0.5,
-              color: "rgba(0,0,0,0)"
-            },
-            {
-              offset: 1,
-              color: "#4BFFFC"
-            }
-          ],
-          global: false
-        },
-        opacity: 1 // 区域透明度
+        color: "#7bbfea",
+        opacity: 0.7 // 区域透明度
       }
     }
   ]
-  const colorArr = ["#4A99FF", "#4BFFFC"] //颜色
   return {
-    color: colorArr,
-    legend: {
-      orient: "vertical",
-      icon: "circle", //图例形状
-      bottom: 35,
-      right: 40,
-      itemWidth: 14, // 图例标记的图形宽度。[ default: 25 ]
-      itemHeight: 14, // 图例标记的图形高度。[ default: 14 ]
-      itemGap: 21, // 图例每项之间的间隔。[ default: 10 ]横向布局时为水平间隔，纵向布局时为纵向间隔。
-      textStyle: {
-        fontSize: 14,
-        color: "#00E4FF"
-      }
+    tooltip:{
+      show: true,
+      trigger:"item",
     },
     radar: {
-      indicator: indicator,
+      indicator,
       splitArea: {
         // 坐标轴在 grid 区域中的分隔区域，默认不显示。
         show: true,
@@ -132,21 +74,23 @@ export const radarEchartOption = (): EChartsOption => {
       axisLine: {
         //指向外圈文本的分隔线样式
         lineStyle: {
-          color: "#153269"
+          color: "rgba(127, 149, 157, 0.19)"
         }
       },
       splitLine: {
         lineStyle: {
-          color: "#113865", // 分隔线颜色
-          width: 1 // 分隔线线宽
+          color: "rgba(127, 149, 157, 0.19)" // 分隔线颜色
         }
+      },
+      axisName: {
+        color: "rgba(74, 186, 230, 1)",
+        fontWeight: "bolder"
       }
     },
     series: [
       {
         type: "radar",
         symbolSize: 8,
-        // symbol: 'angle',
         data: dataArr
       }
     ]
