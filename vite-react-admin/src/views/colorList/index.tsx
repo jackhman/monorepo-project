3158f9ce0f12783c8e5ca01710e7ff5a4697996a
zhao.liu <zhao.liu@wayz.ai>
2024-04-16 09:23:10 +0800

    :fire: 添加一个页面，用来显示颜色表大全，同时可以点击之后就复制

--- vite-react-admin/src/views/colorList/index.tsx
+++ vite-react-admin/src/views/colorList/index.tsx
@@ -1,5 +1,37 @@
+import { Col, Row, message } from "antd"
+import colorOptions from "./colorOptions"
+import styles from "./index.module.scss"
+import { useRef } from "react"
 const ColorList = () => {
-  return <></>
+  const textRef = useRef("")
+  function colorClick(item: { label: string; color: string }) {
+    textRef.current = item.color
+    const text = textRef.current
+    navigator.clipboard.writeText(text)
+    message.success(`${item.label}的十六进制颜色${item.color}复制成功 🎉`)
+  }
+  return (
+    <Row gutter={5}>
+      {colorOptions.map((item, index) => {
+        return (
+          <Col key={index} span={4}>
+            <div
+              className={styles["color-item"]}
+              onClick={() => colorClick(item)}
+            >
+              <span>{item.label}</span>
+              <span
+                className={styles["color"]}
+                style={{ backgroundColor: item.color }}
+              >
+                {item.color}
+              </span>
+            </div>
+          </Col>
+        )
+      })}
+    </Row>
+  )
 }
 
 export default ColorList
