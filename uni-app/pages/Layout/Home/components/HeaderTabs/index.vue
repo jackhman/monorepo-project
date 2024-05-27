2eb2e429ab22227cf2455c63df06c84b28c1cfc3
zhao.liu <zhao.liu@wayz.ai>
2024-05-27 09:56:50 +0800

    :art: 首页添加二级菜单的静态布局

---
+++ uni-app/pages/Layout/Home/components/HeaderTabs/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <view>
+    <u-tabs
+      :list="list"
+      lineWidth="30"
+      lineColor="#f56c6c"
+      :activeStyle="{
+        color: '#303133',
+        fontWeight: 'bold',
+        transform: 'scale(1.05)'
+      }"
+      :inactiveStyle="{
+        color: '#606266',
+        transform: 'scale(1)'
+      }"
+      itemStyle="padding-left: 15px; padding-right: 15px; height: 34px;"
+    >
+    </u-tabs>
+  </view>
+</template>
+<script>
+export default {
+  name: "HeaderTabs",
+  data() {
+    return {
+      list: [
+        {
+          name: "关注"
+        },
+        {
+          name: "推荐",
+          badge: {
+            isDot: true
+          }
+        },
+        {
+          name: "电影"
+        },
+        {
+          name: "科技"
+        },
+        {
+          name: "音乐"
+        },
+        {
+          name: "美食"
+        },
+        {
+          name: "文化"
+        },
+        {
+          name: "财经"
+        },
+        {
+          name: "手工"
+        }
+      ]
+    }
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
--- uni-app/pages/Layout/Home/index.vue
+++ uni-app/pages/Layout/Home/index.vue
@@ -1,25 +1,28 @@
 <template>
   <view class="home-box">
     <SearchCom homeSearch @inputClick="searchClick"></SearchCom>
+    <HeaderTabs></HeaderTabs>
   </view>
 </template>
 
 <script>
+import HeaderTabs from "./components/HeaderTabs/index.vue"
 export default {
   name: "HomeCom",
+  components: {
+    HeaderTabs
+  },
   data() {
     return {}
   },
   methods: {
     searchClick() {
       uni.navigateTo({
-      	url:"/pages/Search/Search"
+        url: "/pages/Search/Search"
       })
-    },
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
